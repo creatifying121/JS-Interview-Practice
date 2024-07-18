098b0d613483f2c38751ba6d384776737905af81
creatifying121 <shivani.raichandani121@gmail.com>
2024-07-18 20:46:17 +0530

    promise overview commit

--- index.js
+++ index.js
@@ -334,3 +334,18 @@ const opReduce = users.reduce(function (acc, curr) {
 console.log(opReduce);
 
 // =====================================================================================
+
+// ==================================== Promises =======================================
+
+// 1. Before promise we used to depend on callback functions which would result in 1.) Callback Hell (Pyramid of doom) | 2.) Inversion of control
+// 2. Inversion of control is overcome by using promise.
+//   2.1) A promise is an object that represents eventual completion/failure of an asynchronous operation.
+//   2.2) A promise has 3 states: pending | fulfilled | rejected.
+//   2.3)  As soon as promise is fulfilled/rejected => It updates the empty object which is assigned undefined in pending state.
+//   2.4) A promise resolves only once and it is immutable.
+//   2.5) Using .then() we can control when we call the cb(callback) function.
+
+// 3. To avoid callback hell (Pyramid of doom) => We use promise chaining. This way our code expands vertically instead of horizontally. Chaining is done using '.then()'
+// 4. A very common mistake that developers do is not returning a value during chaining of promises. Always remember to return a value. This returned value will be used by the next .then()
+
+// =====================================================================================
